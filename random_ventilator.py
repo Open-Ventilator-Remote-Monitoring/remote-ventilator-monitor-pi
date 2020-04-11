85f7413fc37b80b959e58a59b53f728af5763a8f
George Walter Colgrove IV <georgecolgrove@outlook.com>
2020-04-11 19:50:00 -0400

    improve checksum check

--- random_ventilator.py
+++ random_ventilator.py
@@ -12,12 +12,6 @@ class RandomVentilator(VentilatorCommunication):
               "Breathing rate too high", "Breathing rate too low", "Cross-contamination", "Disconnect",
               "Component Malfunction", "Failure"]
 
-    def __init__(self, link: str, baud: int, timeout: int) -> None:
-        print(f'RandomVentilator configured with {link} at baud {baud} with timeout {timeout}')
-
-    def start_connection(self) -> None:
-        pass
-
     def get_data(self) -> VentilatorData:
         return VentilatorData(
             tidal_volume=random.randint(100, 1500),
