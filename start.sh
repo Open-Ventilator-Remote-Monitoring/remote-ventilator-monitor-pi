b535bbe78f3a894cb47063e2df07b0a9b063289c
samcervantes <cervantes.samuel@gmail.com>
2020-04-21 18:39:12 +0100

    update start.sh and application-development.yml

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ PORT="${FLASK_PORT:-8080}"
 LISTEN="${FLASK_LISTEN:-0.0.0.0}"
 echo environment ${FLASK_ENV}
 echo listening on ${LISTEN}:${PORT}
-gunicorn --certfile server.crt --keyfile server.key --bind ${LISTEN}:${PORT} 'wsgi:create_app()'
+gunicorn --certfile ../server.crt --keyfile ../server.key --bind ${LISTEN}:${PORT} 'wsgi:create_app()'
